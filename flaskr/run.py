66e964d0d4a9c518d9a7093b54e05260829356a1
BitPhil <philipp.soehring@gmail.com>
2019-08-02 14:09:40 +0200

    bugfixing

--- flaskr/run.py
+++ flaskr/run.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,11 +6,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/index.html')
+def home():
+    return render_template('index.html')
+
+@app.route('/alarms.html')
 def alarms():
     return render_template('alarms.html')
 
+@app.route('/settings.html')
 def settings():
     return render_template('settings.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=80, host='0.0.0.0')
+    app.run(debug=True, port=80, host='192.168.178.31')
+
