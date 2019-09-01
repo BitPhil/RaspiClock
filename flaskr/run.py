50e97671d9c42dfa868de2ef66c03e69f7948e29
BitPhil <you@example.com>
2019-09-01 16:19:32 +0200

    added checkmarks to proof communication

--- flaskr/run.py
+++ flaskr/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
 import requests, json, time
 
 app = Flask(__name__)
@@ -21,28 +21,26 @@ def settings():
 
 @app.route('/_light', methods=['POST'])
 def lightOn():
-    if(request.method == 'POST'):
-        status_on = request.form['status_on']
+    status_on = request.form['status_on']
+    flash("status_on:", status_on)
 
-        # Setting up URL to send commands to the bulb via the hue bridge
-        url = "http://192.168.178.28/api/QpwEJGEz2Vw6J6z66yj2vRhC4yruI6sMr8jOnyZe/lights/4/state"
+    # Setting up URL to send commands to the bulb via the hue bridge
+    url = "http://192.168.178.28/api/QpwEJGEz2Vw6J6z66yj2vRhC4yruI6sMr8jOnyZe/lights/4/state"
 
-        # Setting up the command/data based on content of status_on (true or false)
-        if(status_on == True):
-            data = {"on":True, "bri":200} #bri = 254 is full brightness
-            response = {'turned on'}
-        else:
-            data = {"on":False}
-            response = {'turned off'}
+    # Setting up the command/data based on content of status_on (true or false)
+    if(status_on == True):
+        data = {"on":True, "bri":200} #bri = 254 is full brightness
+        response = {'turned on'}
+    else:
+        data = {"on":False}
+        response = {'turned off'}
 
-        # Send the command to the bulb via hue bridge
-        r = requests.put(url, json.dumps(data), timeout=5)
+    flash(response)
+    
+    # Send the command to the bulb via hue bridge
+    r = requests.put(url, json.dumps(data), timeout=5)
 
-
-        return response
-
-        #after that go back to the home page
-        #return redirect(url_for('index'), code=302)    
+    return response    
 
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='192.168.178.31')
